ee4eaddc36f0b05c1fa100ca586c74238c289478
ericahicks <48265629+ericahicks@users.noreply.github.com>
2024-01-26 13:28:07 -0500

    first commit

---
+++ src/com/skillstorm/Hello.java
@@ -0,0 +1,54 @@
+package com.skillstorm;
+
+/*
+ * This class contains the main method that prints out the greeting methods of each person.
+ * Each method is named after the person who is greeting themselves.
+ * 
+ * The purpose of this code is to practice git branching, git merging, and Pull Requests. 
+ */
+public class Hello {
+	
+	public static void main(String[] args) {
+		// TODO implement the method named after you 
+		// Have the method return a String containing a greeting message
+		// Call your method without overwriting anyone else's method call
+		erica();
+	}
+	
+	private static String erica() {
+		return "Hello! This is Erica.";
+	}
+	
+	private static String abdelnasser() {
+		return "";
+	}
+	
+	private static String carlos() {
+		return "";
+	}
+	
+	private static String hunter() {
+		return "";
+	}
+	
+	private static String josh() {
+		return "";
+	}
+	
+	private static String matt() {
+		return "";
+	}
+	
+	private static String rod() {
+		return "";
+	}
+	
+	private static String sam() {
+		return "";
+	}
+	
+	private static String sean() {
+		return "";
+	}
+
+}
